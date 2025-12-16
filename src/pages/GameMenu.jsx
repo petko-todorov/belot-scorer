a024d460c3df1d6e677c1535bfefc29efb343f64
Petko Todorov <p.todorov1015@gmail.com>
2025-12-16 20:13:25 +0200

    Add game list functionality to GameMenu

--- src/pages/GameMenu.jsx
+++ src/pages/GameMenu.jsx
@@ -1,7 +1,64 @@
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { getGames } from '../utils/storage';
+
 const GameMenu = () => {
+    const navigate = useNavigate();
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        const storedGames = getGames();
+        setGames(storedGames);
+    }, []);
+
+    const newGame = () => {
+        const id = `${new Date().toLocaleDateString('bg-BG')}-${Date.now()}`;
+        navigate(`/game/${id}`);
+    };
+
+    const continueGame = (id) => {
+        navigate(`/game/${id}`);
+    };
+
     return (
         <>
-            <h1>Game Menu</h1>
+            <div className="flex justify-between items-center px-4 pt-5 pb-1.5 text-xl">
+                <Link to="/">‹Назад</Link>
+                <h1>Игри</h1>
+                <button onClick={newGame}>Нова игра</button>
+            </div>
+
+            <hr />
+
+            <ul>
+                {games.map((game) => (
+                    <li
+                        key={game.id}
+                        onClick={() => continueGame(game.id)}
+                        className="select-none"
+                    >
+                        <div className="mx-4">
+                            <div className="flex justify-between items-center text-2xl">
+                                <h3>Игра</h3>
+                                <span>{game.date}</span>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <h3 className="text-xl">Ние</h3>
+                                <span className="font-bold text-3xl">
+                                    {game.totalUs}
+                                </span>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <h3 className="text-xl">Вие</h3>
+                                <span className="font-bold text-3xl">
+                                    {game.totalThem}
+                                </span>
+                            </div>
+                            <hr className="max-w-screen -mx-4 border-t-2 border-gray-300" />
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 };
