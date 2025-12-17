ae8e225eb6e07dedbf2b5f48f37eadfed16ddd10
Petko Todorov <p.todorov1015@gmail.com>
2025-12-17 15:03:37 +0200

    Update heading and paragraph font styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ const Home = () => {
     return (
         <>
             <main className="text-center pt-12">
-                <h1 className="text-5xl mb-20">Belot Scorer 2</h1>
+                <h1 className="text-6xl mb-20 font-casual">Belot Scorer 2</h1>
 
-                <p className="text-2xl">
+                <p className="text-2xl font-zakirah">
                     <a href="/menu">Game Menu</a>
                 </p>
             </main>
