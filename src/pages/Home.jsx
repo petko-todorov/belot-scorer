12d3efac225bf7e3f89f14d21394727faf134f5e
Petko Todorov <p.todorov1015@gmail.com>
2025-12-17 18:50:42 +0200

    Update font classes in Home page
    Replaces 'font-casual' and 'font-zakirah' with 'font-custom' for the heading and paragraph in Home.jsx to standardize font usage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ const Home = () => {
     return (
         <>
             <main className="text-center pt-12">
-                <h1 className="text-6xl mb-20 font-casual">Belot Scorer 2</h1>
+                <h1 className="text-6xl mb-20 font-custom">Belot Scorer 2</h1>
 
-                <p className="text-2xl font-zakirah">
+                <p className="text-2xl font-custom">
                     <a href="/menu">Game Menu</a>
                 </p>
             </main>
