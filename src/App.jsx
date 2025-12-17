fcdba90818e1926d57486f75598ce1485d21bfb5
Petko Todorov <p.todorov1015@gmail.com>
2025-12-17 18:52:10 +0200

    Remove unnecessary wrapper div in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import AppRoutes from './routes';
 
 function App() {
-    return (
-        <div className="bg-zinc-400 max-w-1/2 min-h-screen max-h-screen overflow-auto mx-auto">
-            <AppRoutes />
-        </div>
-    );
+    return <AppRoutes />;
 }
 
 export default App;
