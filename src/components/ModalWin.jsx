041e211b243bce31e08101ec603117ba80e725bd
Petko Todorov <p.todorov1015@gmail.com>
2025-12-21 13:06:33 +0200

    Initial commit

---
+++ src/components/ModalWin.jsx
@@ -0,0 +1,42 @@
+import { Modal, Box, Typography, Button } from '@mui/material';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const ModalWin = ({ openModal, confirmEndGame, cancelEndGame }) => {
+    return (
+        <>
+            <Modal
+                open={openModal}
+                onClose={(event, reason) => {
+                    if (reason === 'backdropClick') return;
+                }}
+                disableEscapeKeyDown
+            >
+                <Box sx={style}>
+                    <Typography variant="h6">Свърши ли играта?</Typography>
+
+                    <Box className="flex justify-center gap-3 mt-3">
+                        <Button variant="outlined" onClick={cancelEndGame}>
+                            Не
+                        </Button>
+                        <Button variant="contained" onClick={confirmEndGame}>
+                            Да
+                        </Button>
+                    </Box>
+                </Box>
+            </Modal>
+        </>
+    );
+};
+
+export default ModalWin;
