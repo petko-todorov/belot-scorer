c8531e4a959d97d2b8e61f9591abea633064baa2
Petko Todorov <p.todorov1015@gmail.com>
2025-12-22 15:12:02 +0200

    Add responsive grid layout to ScoreLine component

--- src/components/GameHistory.jsx
+++ src/components/GameHistory.jsx
@@ -43,7 +43,7 @@ const GameHistory = ({ games }) => {
 };
 
 const ScoreLine = ({ prev, value }) => (
-    <div className="grid grid-cols-[4ch_2ch_4ch] justify-center">
+    <div className="grid grid-cols-[4ch_2ch_4ch] justify-center max-sm:grid-cols-[1fr_0.5fr_1fr]">
         <span className="text-right">{prev}</span>
         <span className="text-center">-</span>
         <span className="text-left">{value}</span>
