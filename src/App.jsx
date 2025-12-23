22198620ca915b879628fab19a82409621221b0f
Petko Todorov <p.todorov1015@gmail.com>
2025-12-23 10:44:10 +0200

    Configure native status bar appearance

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import AppRoutes from './routes';
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 function App() {
+    useEffect(() => {
+        if (Capacitor.isNativePlatform()) {
+            StatusBar.setBackgroundColor({ color: '#ededed' });
+            StatusBar.setStyle({ style: Style.Dark });
+        }
+    }, []);
     return <AppRoutes />;
 }
 
