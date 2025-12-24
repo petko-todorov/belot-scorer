fd820102eef636258ceb36e2fab1cf43f27a7315
Petko Todorov <p.todorov1015@gmail.com>
2025-12-24 20:44:34 +0200

    Make ScoreInput responsive for smaller screens

--- src/components/ScoreInput.jsx
+++ src/components/ScoreInput.jsx
@@ -2,19 +2,21 @@ import { clampScore } from '../utils/score';
 
 const ScoreInput = ({ total, value, onChange, inputRef, onEnter }) => {
     return (
-        <div className="grid grid-cols-[4ch_2ch_4ch] justify-center">
+        <div className="grid grid-cols-[4ch_2ch_4ch] justify-center max-sm:grid-cols-[1fr_0.5fr_1fr]">
             <span className="text-right">{total}</span>
             <span className="text-center">-</span>
-            <input
-                ref={inputRef}
-                type="tel"
-                value={value}
-                inputMode="numeric"
-                className="w-[3ch] bg-transparent text-left outline-none"
-                placeholder="0"
-                onChange={(e) => onChange(clampScore(e.target.value))}
-                onKeyDown={(e) => e.key === 'Enter' && onEnter()}
-            />
+            <span className="text-left">
+                <input
+                    ref={inputRef}
+                    type="tel"
+                    value={value}
+                    inputMode="numeric"
+                    className="w-[3ch] bg-transparent outline-none"
+                    placeholder="0"
+                    onChange={(e) => onChange(clampScore(e.target.value))}
+                    onKeyDown={(e) => e.key === 'Enter' && onEnter()}
+                />
+            </span>
         </div>
     );
 };
