7f6031ff93e30b56bf7caeb0ecc8e0143b0b6268
Petko Todorov <p.todorov1015@gmail.com>
2025-12-24 11:14:39 +0200

    Remove unused BrowserRouter import

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import Home from '../pages/Home';
 import Menu from '../pages/Menu';
 import Game from '../pages/Game';
 
 const AppRoutes = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/game/:id" element={<Game />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/game/:id" element={<Game />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
     );
 };
 
